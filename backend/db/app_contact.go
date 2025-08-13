99869471a1b32780e7dc3fd9ccccd8905e56d1ed
PJ O'Leary <peej@oleary.com>
2025-08-13 13:31:57 -0400

    contact scaffolding

---
+++ backend/db/app_contact.go
@@ -0,0 +1,17 @@
+package db
+
+func (a *AppDB) AddContact(userId string, name string, address string) {
+
+}
+
+func (a *AppDB) UpdateContact(userId string, contactId string, name string, address string) {
+
+}
+
+func (a *AppDB) GetContacts(userId string) {
+
+}
+
+func (a *AppDB) DeleteContact(userId string, contactId string) {
+
+}
